6bf5acc949c789911d4381830d2070916bb1e2f9
Bruno <brunov.carvalho94@gmail.com>
2019-08-19 18:07:14 -0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import API from "./api";
 function App() {
+  const [hitsPerPage, setHitsPerPage] = useState(0);
+  let [hits, setHits] = useState(0);
+
+  useEffect(async () => {
+    const result = await API.get(
+        'https://hn.algolia.com/api/v1/search?query=redux'
+    )
+
+    setHits(result.data.hitsPerPage);
+  }, []);
+
+  
+  useEffect(() => {
+    testA();
+
+  }, [hitsPerPage, hits]);
+
+  function testA()  {
+    setTimeout(() => {
+      if (hitsPerPage < hits) {
+        setHitsPerPage(hitsPerPage + 1);
+      }
+    }, calcTime());
+  }
+
+  function calcTime() {
+    return 500 / hits ;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {hitsPerPage}
         </p>
         <a
           className="App-link"
