e617ec40a6585a698098200acd2db12e225dff7f
Bruno <brunov.carvalho94@gmail.com>
2019-09-02 18:34:04 -0300

    Add form

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
 import logo from './logo.svg';
 import './App.css';
 import API from "./api";
 function App() {
-  const [hitsPerPage, setHitsPerPage] = useState(0);
-  let [hits, setHits] = useState(0);
+  // const [hitsPerPage, setHitsPerPage] = useState(0);
+  // let [hits, setHits] = useState(0);
 
-  useEffect(async () => {
-    const result = await API.get(
-        'https://hn.algolia.com/api/v1/search?query=redux'
-    )
+  // useEffect(async () => {
+  //   const result = await API.get(
+  //       'https://hn.algolia.com/api/v1/search?query=redux'
+  //   )
 
-    setHits(result.data.hitsPerPage);
-  }, []);
+  //   setHits(result.data.hitsPerPage);
+  // }, []);
 
-  
-  useEffect(() => {
-    testA();
-
-  }, [hitsPerPage, hits]);
-
-  function testA()  {
-    setTimeout(() => {
-      if (hitsPerPage < hits) {
-        setHitsPerPage(hitsPerPage + 1);
-      }
-    }, calcTime());
-  }
-
-  function calcTime() {
-    return 500 / hits ;
-  }
+ 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {hitsPerPage}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
     </div>
   );
 }
